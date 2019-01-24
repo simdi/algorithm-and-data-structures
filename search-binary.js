7de9e7434104d47806929f4306eece9451f27035
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-01-24 23:10:04 +0100

    Done with binary search.

---
+++ search-binary.js
@@ -0,0 +1,24 @@
+/*
+* @author: Chisimdi Damian Ezeanieto
+* @date: 24/01/2019
+*/
+function binarySearch(arr, item) {
+    arr = arr.sort((a,b) => a-b);
+    let left = 0;
+    let right = arr.length - 1;
+    let middle = Math.floor((left + right) / 2);
+    while (arr[middle] !== item && left <= right) {
+        (arr[middle] > item) ? right = middle - 1 : left = middle + 1;
+        middle = Math.floor((left + right) / 2);
+    }
+
+    if (arr[middle] === item) {
+        return middle;
+    } {
+        return -1;
+    }
+}
+
+console.log(binarySearch([2,5,6,7,8,23,45,87,100,86,37], 46));
+// [2,5,6,7,8,23,87,100,86,37,45]
+//  0          5                 11
