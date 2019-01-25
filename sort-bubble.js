59118869f7e342ab2a3f88f5e011e2b418b37a43
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-01-25 11:24:47 +0100

    Optimized bubble sort.

--- sort-bubble.js
+++ sort-bubble.js
@@ -16,19 +16,37 @@
 //     }
 //     return arr;
 // };
-// Optimized solution
+// Optimized solution one
+// const bubbleSort = (arr) => {
+//     for (let i = arr.length; i > 0; i--) {
+//         for (let j = 0; j < (i - 1); j++) {
+//             console.log(arr[j], arr[j+1]);
+//             if (arr[j] > arr[j+1]) {
+//                 // Swap
+//                 [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+//             }
+//         }
+//         console.log('One pass complete');
+//     }
+//     return arr;
+// };
+// Optimized solution one
 const bubbleSort = (arr) => {
+    let noSwaps = false;
     for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
         for (let j = 0; j < (i - 1); j++) {
             console.log(arr[j], arr[j+1]);
             if (arr[j] > arr[j+1]) {
                 // Swap
                 [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+                noSwaps = false;
             }
         }
+        if (noSwaps) break;
         console.log('One pass complete');
     }
     return arr;
 };
 
-console.log(bubbleSort([3,1,5,7,2,8,34,9]));
+console.log(bubbleSort([34,1,3,5,7,8,9]));
