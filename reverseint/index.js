6502ec40908e8a95778d0bba136ace6d1fc74de1
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-02-19 00:04:10 +0100

    Optimised reverseint method.

--- reverseint/index.js
+++ reverseint/index.js
@@ -17,14 +17,24 @@
 const reverseInt = (int) => {
     // convert the integer into a string.
     str = int.toString();
+    // Convert the string into an array.
+    str = str.split('');
     // Naive algorithm.
     // Big(O) = N
-    let reversedStr = '';
-    for(let char of str) {
-        reversedStr = char + reversedStr;
+    // let reversedStr = '';
+    // for(let char of str) {
+    //     reversedStr = char + reversedStr;
+    // }
+
+    // return parseInt(reversedStr) * Math.sign(int);
+
+    // Switch positions as I loop through the string.
+    // Big(O) = log N
+    for (let i = 0; i < Math.floor(str.length/2); i++) {
+        [str[i], str[str.length-1-i]] = [str[str.length-1-i], str[i]];
     }
 
-    return parseInt(reversedStr) * Math.sign(int);
+    return parseInt(str.join('')) * Math.sign(int);
 };
 
 module.exports = reverseInt;
