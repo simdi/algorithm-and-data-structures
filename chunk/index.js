b84c7395b588481765701012f43662cf086ec4ef
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-03-05 16:48:54 +0100

    Done with a better solution of the chunk arra.

--- chunk/index.js
+++ chunk/index.js
@@ -4,21 +4,31 @@
 */
 
 const chunk = (arr, size) => {
-    let result = [];
-    let chunk = [];
+    const result = [];
+    // let chunk = [];
 
     // Big (O) = N;
-    for (let i = 0; i < arr.length; i++) {
-        chunk.push(arr[i]);
-        if (chunk.length === size) {
-            result.push(chunk);
-            chunk = [];
-        }
+    // for (let i = 0; i < arr.length; i++) {
+    //     chunk.push(arr[i]);
+    //     if (chunk.length === size) {
+    //         result.push(chunk);
+    //         chunk = [];
+    //     }
 
-        if ((arr.length-1 === i) && chunk.length > 0) {
-            result.push(chunk);
-        }
+    //     if ((arr.length-1 === i) && chunk.length > 0) {
+    //         result.push(chunk);
+    //     }
+    // }
+
+    // Performant 
+    // Big (O) = Log N;
+    const recursion = (array) => {
+        // Base case
+        if (array.length === 0) return;
+        result.push(array.splice(0, size));
+        recursion(array);
     }
+    recursion(arr);
 
     return result;
 }
