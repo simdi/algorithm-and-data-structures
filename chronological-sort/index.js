b152b6b9b7fe4bec3173240c482fc4dce62a00d5
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-01-22 16:53:59 +0100

    Sorted files.

--- chronological-sort/index.js
+++ chronological-sort/index.js
@@ -4,14 +4,14 @@
 */
 
 const chronologicalSort = (arr, order = 'old') => {
-    if (arr.length === 0) return false;
-    return arr.sort((a, b) => {
-        if(order === 'old') {
-            return new Date(b).getTime() - new Date(a).getTime();
-        } else {
-            return new Date(a).getTime() - new Date(b).getTime();
-        }
-    });
+  if (arr.length === 0) return false;
+  return arr.sort((a, b) => {
+    if(order === 'old') {
+        return new Date(b).getTime() - new Date(a).getTime();
+    } else {
+        return new Date(a).getTime() - new Date(b).getTime();
+    }
+  });
 }
 
 module.exports = chronologicalSort;
