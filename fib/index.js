9751e48d5e9a77ef547656b3ad0f37b6b34a3e78
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-06-23 06:00:06 +0100

    Updated fibonacci series to contain memoization.

--- fib/index.js
+++ fib/index.js
@@ -5,25 +5,24 @@
 
 // Fibonacci Series
 const fibonacci = (n, cache) => {
-    // // 0 (n^2) not performant
+  // // 0 (n^2) not performant
+  // if (n < 3) {
+  //   return 1;
+  // } else {
+  //   return fibonacci(n - 2) + fibonacci(n - 1);
+  // }
+
+  // O(n) performant
+  cache = cache || [];
+  if (cache[n]) return cache[n];
+  else {
     if (n < 3) {
-        return 1;
+      return 1;
     } else {
-        return fibonacci(n - 2) + fibonacci(n - 1);
+      cache[n] = fibonacci(n - 2, cache) + fibonacci(n - 1, cache);
     }
-
-    // // O(n) performant
-    // cache = cache || [];
-    // if (cache[idx]) return cache[idx];
-    // else {
-    //   if (idx < 3) {
-    //     return 1;
-    //   } else {
-    //     cache[idx] = fibonacci(idx - 2, cache) + fibonacci(idx - 1, cache);
-    //   }
-    // } 
-
-    // return cache[idx];
+  }
+  return cache[n];
 };
 
 module.exports = fibonacci;
--- fib/test.js
+++ fib/test.js
@@ -4,21 +4,23 @@
 */
 const fib = require('./index');
 
-test('Fib function is defined', () => {
-  expect(typeof fib).toEqual('function');
+describe('Fibonacci Tests', () => {
+  test('Fib function is defined', () => {
+    expect(typeof fib).toEqual('function');
+  });
+  it('Calculate correct fib value for 1 === 1', async() => {
+    expect(fib(1)).toEqual(1);
+  });
+  it('Calculate correct fib value for 2 === 1', async() => {
+    expect(fib(2)).toEqual(1);
+  });
+  it('Calculate correct fib value for 5 === 5', async() => {
+    expect(fib(5)).toEqual(5);
+  });
+  test('Calculate correct fib value for 6 === 8', async() => {
+    expect(fib(6)).toEqual(8);
+  });
+  test('Calculate correct fib value for 50 === 12586269025', async() => {
+    expect(fib(50)).toEqual(12586269025);
+  });
 });
-test('Calculate correct fib value for 1 === 1', () => {
-  expect(fib(1)).toEqual(1);
-});
-test('Calculate correct fib value for 2 === 1', () => {
-  expect(fib(2)).toEqual(1);
-});
-test('Calculate correct fib value for 5 === 5', () => {
-  expect(fib(5)).toEqual(5);
-});
-test('Calculate correct fib value for 6 === 8', () => {
-  expect(fib(6)).toEqual(8);
-});
-// test('Calculate correct fib value for 50 === 12586269025', _=> {
-//   expect(fib(50)).toEqual(12586269025);
-// });
