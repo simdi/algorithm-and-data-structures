7a3596f07c175ab5b357a1a40755895fcf2c0f45
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-06-23 05:30:44 +0100

    Uptated package.json.

--- fib/test.js
+++ fib/test.js
@@ -5,20 +5,20 @@
 const fib = require('./index');
 
 test('Fib function is defined', () => {
-    expect(typeof fib).toEqual('function');
+  expect(typeof fib).toEqual('function');
 });
 test('Calculate correct fib value for 1 === 1', () => {
-    expect(fib(1)).toEqual(1);
+  expect(fib(1)).toEqual(1);
 });
 test('Calculate correct fib value for 2 === 1', () => {
-    expect(fib(2)).toEqual(1);
+  expect(fib(2)).toEqual(1);
 });
 test('Calculate correct fib value for 5 === 5', () => {
-    expect(fib(5)).toEqual(5);
+  expect(fib(5)).toEqual(5);
 });
 test('Calculate correct fib value for 6 === 8', () => {
-    expect(fib(6)).toEqual(8);
+  expect(fib(6)).toEqual(8);
 });
 // test('Calculate correct fib value for 50 === 12586269025', _=> {
-//     expect(fib(50)).toEqual(12586269025);
+//   expect(fib(50)).toEqual(12586269025);
 // });
