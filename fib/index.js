8a1fe52c6a530c0d9d494b5037d73f18a2a428d3
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-06-23 06:26:45 +0100

    Fibonacci series using loop.

--- fib/index.js
+++ fib/index.js
@@ -12,17 +12,30 @@ const fibonacci = (n, cache) => {
   //   return fibonacci(n - 2) + fibonacci(n - 1);
   // }
 
-  // O(n) performant
-  cache = cache || [];
-  if (cache[n]) return cache[n];
-  else {
-    if (n < 3) {
-      return 1;
-    } else {
-      cache[n] = fibonacci(n - 2, cache) + fibonacci(n - 1, cache);
-    }
+  // O(n) using recursion
+  // cache = cache || [];
+  // if (cache[n]) return cache[n];
+  // else {
+  //   if (n < 3) {
+  //     return 1;
+  //   } else {
+  //     cache[n] = fibonacci(n - 2, cache) + fibonacci(n - 1, cache);
+  //   }
+  // }
+  // return cache[n];
+
+  // O(n) using loop and memoization
+  const mem = [];
+  mem[0] = 0;
+  mem[1] = 1;
+  mem[2] = 1;
+  let i = 3;
+  while(i <= n) {
+    mem[i] = mem[i-1] + mem[i-2];
+    i++;
   }
-  return cache[n];
+
+  return mem[n];
 };
 
 module.exports = fibonacci;
