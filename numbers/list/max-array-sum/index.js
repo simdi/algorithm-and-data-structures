3bf8035818ce1b59e4436e0165dfd72afdcb283c
Chisimdi Ezeanieto <simdi4iverson@yahoo.co.uk>
2023-04-10 14:49:12 +0100

    Updated maxSubArraySum.

--- numbers/list/max-array-sum/index.js
+++ numbers/list/max-array-sum/index.js
@@ -4,21 +4,41 @@
 */
 
 const maxSubArraySum = (arr, size) => {
-  if (arr.length === 0 || size > arr.length) return null;
-  let i = 0;
-  let j = size - 1;
-  let sum = -Infinity;
+  // if (arr.length === 0 || size > arr.length) return null;
+  // let i = 0;
+  // let j = size - 1;
+  // let sum = -Infinity;
 
-  while (j < arr.length) {
-    const total = arr.slice(i, j+1);
-    const sumTotal = total.reduce((a, b) => a + b);
-    if (sumTotal > sum) {
-      sum = sumTotal;
-    }
-    i++;
-    j++;
+  // while (j < arr.length) {
+  //   const total = arr.slice(i, j+1);
+  //   const sumTotal = total.reduce((a, b) => a + b);
+  //   if (sumTotal > sum) {
+  //     sum = sumTotal;
+  //   }
+  //   i++;
+  //   j++;
+  // }
+  // return sum;
+
+  // Another solution
+  // This solutions performance is O(n)
+  if (arr.length === 0 || size > arr.length) return null;
+  let tempSum = 0;
+  // Add the first size elements of the array
+  for (let i = 0; i < size; i++) {
+    tempSum += arr[i];
   }
-  return sum;
+  let maxSum = tempSum;
+  let i = size;
+  // Loop through the elements from the size of the array
+  while (i < arr.length) {
+    // Minus the first element and add the size position of the array
+    tempSum = tempSum - arr[i - size] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+    i++;
+  }
+  return maxSum;
 };
+
 
 module.exports = maxSubArraySum;
--- numbers/list/max-array-sum/test.js
+++ numbers/list/max-array-sum/test.js
@@ -4,17 +4,20 @@
 */
 const maxSubArraySum = require('./index');
 test('maxSubArraySum is a function', () => {
-    expect(typeof maxSubArraySum).toEqual('function');
+  expect(typeof maxSubArraySum).toEqual('function');
 });
 test('maxSubArraySum [8,2,54,65,23,7,45] to give 109', () => {
-    expect(maxSubArraySum([8,2,54,65,23,7,45], 2)).toEqual(119);
+  expect(maxSubArraySum([8,2,54,65,23,7,45], 2)).toEqual(119);
 });
 test('maxSubArraySum [1, 9, 11, 13, 2, 3, 7, 5] to give 33', () => {
-    expect(maxSubArraySum([1, 9, 11, 13, 2, 3, 7, 5], 3)).toEqual(33);
+  expect(maxSubArraySum([1, 9, 11, 13, 2, 3, 7, 5], 3)).toEqual(33);
 });
 test('maxSubArraySum [] to give null', () => {
-    expect(maxSubArraySum([], 3)).toEqual(null);
+  expect(maxSubArraySum([], 3)).toEqual(null);
 });
 test('maxSubArraySum [-10,-9,-3,-5,-100,-56,0] to give -17', () => {
-    expect(maxSubArraySum([-10,-9,-3,-5,-100,-56,0], 3)).toEqual(-17);
+  expect(maxSubArraySum([-10,-9,-3,-5,-100,-56,0], 3)).toEqual(-17);
+});
+test('maxSubArraySum [2,6,9,2,1,8,5,6,3] to give 19', () => {
+  expect(maxSubArraySum([2,6,9,2,1,8,5,6,3], 3)).toEqual(19);
 });
